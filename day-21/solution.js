7511a4875a343788f8bc75f1bd2d678f9ef0035d
Karolis Kiaune <karolis.kiaune@kilo.health>
2022-12-21 09:28:29 +0200

    add day 21 part 1 solution

--- day-21/solution.js
+++ day-21/solution.js
@@ -1,6 +1,27 @@
 const fs = require("fs");
+// let list = fs.readFileSync("example.txt", "utf-8");
 let list = fs.readFileSync("input.txt", "utf-8");
 list = list.split(/\r?\n/);
 
-console.log("first part result:");
-console.log("second part result:");
+let monkeysActions = list.reduce((acc, line) => {
+  const [monkey, action] = line.split(": ");
+
+  return {
+    ...acc,
+    [monkey]: /^\d+$/.test(action) ? +action : action,
+  };
+}, {});
+
+console.log("monkeysActions", monkeysActions);
+
+const getMonkeyResult = (monkeyName) => {
+  if (typeof monkeysActions[monkeyName] === "number") {
+    return monkeysActions[monkeyName];
+  }
+
+  const [monkey1, operation, monkey2] = monkeysActions[monkeyName].split(" ");
+
+  return eval(getMonkeyResult(monkey1) + operation + getMonkeyResult(monkey2));
+};
+
+console.log("first part result:", getMonkeyResult("root"));
