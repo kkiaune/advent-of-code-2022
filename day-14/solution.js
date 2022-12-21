fff5f65c6387c37b5d50b24c0be39dfb6cd3aa64
Karolis Kiaune <karolis.kiaune@kilo.health>
2022-12-21 08:48:33 +0200

    add day 14 part 2 solution

--- day-14/solution.js
+++ day-14/solution.js
@@ -2,68 +2,116 @@ const fs = require("fs");
 let list = fs.readFileSync("input.txt", "utf-8");
 
 let mexDepth = 0;
-const board = new Set(
-  list
-    .split(/\r?\n/)
-    .map((line) =>
-      line.split(" -> ").map((point) => point.split(",").map((val) => +val))
-    )
-    .reduce((acc, line) => {
-      for (let i = 0; i < line.length - 1; i++) {
-        let [x1, y1] = line[i];
-        let [x2, y2] = line[i + 1];
-        x1 = +x1;
-        y1 = +y1;
-        x2 = +x2;
-        y2 = +y2;
 
-        for (let x = 0; x < Math.abs(x2 - x1) + 1; x++) {
-          for (let y = 0; y < Math.abs(y2 - y1) + 1; y++) {
-            acc.push(
-              `${x1 > x2 ? x2 + x : x1 + x},${y1 > y2 ? y2 + y : y1 + y}`
-            );
-            mexDepth = Math.max(mexDepth, y1 + y);
+const getBoard = () =>
+  new Set(
+    list
+      .split(/\r?\n/)
+      .map((line) =>
+        line.split(" -> ").map((point) => point.split(",").map((val) => +val))
+      )
+      .reduce((acc, line) => {
+        for (let i = 0; i < line.length - 1; i++) {
+          let [x1, y1] = line[i];
+          let [x2, y2] = line[i + 1];
+          x1 = +x1;
+          y1 = +y1;
+          x2 = +x2;
+          y2 = +y2;
+
+          for (let x = 0; x < Math.abs(x2 - x1) + 1; x++) {
+            for (let y = 0; y < Math.abs(y2 - y1) + 1; y++) {
+              acc.push(
+                `${x1 > x2 ? x2 + x : x1 + x},${y1 > y2 ? y2 + y : y1 + y}`
+              );
+              mexDepth = Math.max(mexDepth, Math.max(y1, y2));
+            }
           }
         }
+        return acc;
+      }, [])
+  );
+
+const getFirstPartResult = () => {
+  const board = getBoard();
+  let sandUnits = 0;
+  let breakSygnal = false;
+
+  while (!breakSygnal) {
+    let sandGrain = [500, 0];
+
+    while (true) {
+      if (sandGrain[1] >= mexDepth) {
+        breakSygnal = true;
+        break;
       }
-      return acc;
-    }, [])
-);
 
-let sandUnits = 0;
-let foundTheVoid = false;
+      // no grain on bottom
+      if (!board.has(`${sandGrain[0]},${sandGrain[1] + 1}`)) {
+        sandGrain[1] += 1;
+        continue;
+      }
 
-while (!foundTheVoid) {
-  let sandGrain = [500, 0];
+      // no grain on the left diagonally
+      if (!board.has(`${sandGrain[0] - 1},${sandGrain[1] + 1}`)) {
+        sandGrain[0] -= 1;
+        continue;
+      }
 
-  while (true) {
-    if (sandGrain[1] >= mexDepth) {
-      foundTheVoid = true;
+      // no grain on the right diagonally
+      if (!board.has(`${sandGrain[0] + 1},${sandGrain[1] + 1}`)) {
+        sandGrain[0] += 1;
+        continue;
+      }
+
+      board.add(`${sandGrain[0]},${sandGrain[1]}`);
+      sandUnits += 1;
       break;
     }
+  }
 
-    // no grain on bottom
-    if (!board.has(`${sandGrain[0]},${sandGrain[1] + 1}`)) {
-      sandGrain[1] += 1;
-      continue;
-    }
+  return sandUnits;
+};
 
-    // no grain on the left diagonally
-    if (!board.has(`${sandGrain[0] - 1},${sandGrain[1] + 1}`)) {
-      sandGrain[0] -= 1;
-      continue;
-    }
+const getSecondPartResult = () => {
+  const board = getBoard();
+  let sandUnits = 0;
 
-    // no grain on the right diagonally
-    if (!board.has(`${sandGrain[0] + 1},${sandGrain[1] + 1}`)) {
-      sandGrain[0] += 1;
-      continue;
+  while (!board.has("500,0")) {
+    let sandGrain = [500, 0];
+
+    while (true) {
+      if (sandGrain[1] >= mexDepth + 1) {
+        break;
+      }
+
+      // no grain on bottom
+      if (!board.has(`${sandGrain[0]},${sandGrain[1] + 1}`)) {
+        sandGrain[1] += 1;
+        continue;
+      }
+
+      // no grain on the left diagonally
+      if (!board.has(`${sandGrain[0] - 1},${sandGrain[1] + 1}`)) {
+        sandGrain[0] -= 1;
+        continue;
+      }
+
+      // no grain on the right diagonally
+      if (!board.has(`${sandGrain[0] + 1},${sandGrain[1] + 1}`)) {
+        sandGrain[0] += 1;
+        continue;
+      }
+
+      break;
     }
 
     board.add(`${sandGrain[0]},${sandGrain[1]}`);
     sandUnits += 1;
-    break;
   }
-}
 
-console.log("first part result:", sandUnits);
+  return sandUnits;
+};
+
+console.log("first part result:", getFirstPartResult());
+console.log("second part result:", getSecondPartResult());
